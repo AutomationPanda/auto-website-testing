52ecc5bda3e81b696bc391721c18a1fc21bbc0e4
Pandy Knight <andylpk.247@gmail.com>
2022-12-27 23:51:25 -0500

    Added env vars to autonomous script

--- autonomous.ts
+++ autonomous.ts
@@ -1,17 +1,35 @@
 import { chromium } from '@playwright/test';
-import { BatchInfo, Configuration, VisualGridRunner, BrowserType, Eyes, Target } from '@applitools/eyes-playwright';
+import { BatchInfo, Configuration, VisualGridRunner, StitchMode, BrowserType, Eyes, Target } from '@applitools/eyes-playwright';
 
 (async () => {
 
+  // Read environment variables
+  const BASE_URL = process.env.BASE_URL;
+  const SITE_NAME = process.env.SITE_NAME;
+  const TEST_CONCURRENCY = Number(process.env.TEST_CONCURRENCY) || 1;
+
+  // Validate environment variables
+  if (!BASE_URL) {
+    throw new Error('ERROR: BASE_URL environment variable is not defined');
+  }
+  if (!SITE_NAME) {
+    throw new Error('ERROR: SITE_NAME environment variable is not defined');
+  }
+
+  // Parse the base and sitemap URLs
+  const baseUrl = BASE_URL.replace(/\/+$/, '');
+  const sitemapUrl = baseUrl + '/sitemap.xml';
+
   // Create Applitools objects
-  let runner = new VisualGridRunner({ testConcurrency: 10 });
-  let batch = new BatchInfo({name: 'Applitools Tutorial Site'});
+  let runner = new VisualGridRunner({ testConcurrency: TEST_CONCURRENCY });
+  let batch = new BatchInfo({name: SITE_NAME});
   let config = new Configuration();
   let eyesClosePromises: Promise<any>[] = [];
 
   // Set Applitools configuration
   config.setBatch(batch);
-  config.setApiKey('');
+  config.setForceFullPageScreenshot(true);
+  config.setStitchMode(StitchMode.CSS);
   config.addBrowser(1600, 1200, BrowserType.CHROME);
   // config.addBrowser(1600, 1200, BrowserType.FIREFOX);
   // config.addBrowser(1600, 1200, BrowserType.SAFARI);
@@ -23,7 +41,7 @@ import { BatchInfo, Configuration, VisualGridRunner, BrowserType, Eyes, Target }
   const page = await context.newPage();
   
   // Get the sitemap
-  await page.goto('https://applitools.com/tutorials/sitemap.xml');
+  await page.goto(sitemapUrl);
   const pageLinks = await page.locator("loc").allTextContents();
 
   // Capture a snapshot for each page
@@ -33,8 +51,8 @@ import { BatchInfo, Configuration, VisualGridRunner, BrowserType, Eyes, Target }
     const eyes = new Eyes(runner, config);
     await eyes.open(
       page,
-      'Applitools Tutorial Site',
-      link.replace('https://applitools.com/tutorials', ''),
+      SITE_NAME,
+      link.replace(baseUrl, ''),
       { width: 1024, height: 768 }
     );
 
